b91a927f92e358b4c24ec653f240e9c0b0663904
Edenilson Fonseca Chagas <edenilsonfc@gmail.com>
2021-02-05 12:12:54 -0300

    Created Navbar, Routes and Home

--- src/App.tsx
+++ src/App.tsx
@@ -1,7 +1,11 @@
 import React from 'react';
+import './core/assests/styles/custom.scss';
+import './app.scss';
+import Routes from './Routes';
 
 const App = () => {
-    return  <h1> Ola mundo</h1>
+    return  (
+        <Routes />
+    );
 }
-
 export default App;
